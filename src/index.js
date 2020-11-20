c4584ea1ea9e8b2be6c9867eaaa443981edb9150
dwanerich <dwanerich@gmail.com>
2020-11-19 19:45:58 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick'
+import LightSwitch from './components/LightSwitch'
 
 ReactDOM.render(
   <div>
     <ClickityClick />
+    <LightSwitch />
   </div>,
   document.getElementById('root')
 );
